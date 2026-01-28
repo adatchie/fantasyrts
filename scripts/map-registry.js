161950090a3d2a48d3bc7bef83d2f6652e70791f
adatchie <adatchie@gmail.com>
2026-01-29 01:51:41 +0900

    Register sample maps (Castle, Mountain) to registry

--- scripts/map-registry.js
+++ scripts/map-registry.js
@@ -1,11 +1,15 @@
 import { TUTORIAL_PLAIN_DATA } from './data/maps/tutorial_plain.js';
+import { SAMPLE_CASTLE_DATA } from './data/maps/sample_castle.js';
+import { SAMPLE_MOUNTAIN_DATA } from './data/maps/sample_mountain.js';
 
 /**
  * プロジェクトに含まれる全マップデータを管理するレジストリ
  * 新しいマップを追加した場合は、ここにimport文と配列への追加を行ってください。
  */
 export const MAP_REGISTRY = [
-    TUTORIAL_PLAIN_DATA
+    TUTORIAL_PLAIN_DATA,
+    SAMPLE_CASTLE_DATA,
+    SAMPLE_MOUNTAIN_DATA
 ];
 
 export class MapRegistry {
